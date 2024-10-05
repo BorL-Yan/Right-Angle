9f61b55ec798f654605d9ffc322e09be692d67a2
Boris <137871704+BorL-Yan@users.noreply.github.com>
2024-10-05 09:21:50 +0400

    V 0.0.2
    I added a rotation function to the object code, when it reaches the correct angle "which is determined from the inspector" the color changes and slowly rearranges.

--- Script/ObjectController.cs
+++ Script/ObjectController.cs
@@ -1,45 +1,94 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectController : MonoBehaviour
 {
+    public TextMeshProUGUI _text;
     
-    public float _rotationSpeed = 100f;
+    [SerializeField] private float _rotationSpeed = 100f;
+    
+    
+    [SerializeField] private float m_valueRightDistance;
+    [SerializeField] private Vector3 m_fromTheRightAngle;
+    [SerializeField] private float m_endGameTimer;
+    
+    [SerializeField] private Color m_startColor;
+    [SerializeField] private Color m_endColor;
+    
+    private Material _material;
+    private bool _isDraging;
+    private bool _endGame;
 
-    void Awaik()
+    private void Start()
     {
-
+        _material = GetComponent<MeshRenderer>().material;
+        _material.SetColor("_Color", m_startColor);
     }
-    
-    
-    private Vector2 _positionMouse = Vector2.zero;
+
     void Update()
     {
-        // if (Input.GetMouseButtonDown(0))
-        // {
-        //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //     _positionMouse = ray.origin;
-        //     
-        //     
-        // }
-
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && Input.touchCount < 1 && !_endGame)
         {
-            // Получаем движение мыши по осям X и Y
-            float mouseX = Input.GetAxis("Mouse X");
-            float mouseY = Input.GetAxis("Mouse Y");
-            
-            // Вращаем куб вокруг оси Y (по горизонтали) и оси X (по вертикали)
-            transform.Rotate(Vector3.up, -mouseX * _rotationSpeed * Time.deltaTime, Space.World);
-            transform.Rotate(Vector3.right, -mouseY * _rotationSpeed * Time.deltaTime, Space.World);
-                
+            RotateObject();
+            _isDraging = true;
         }
-        //this.gameObject.transform.localRotation = Quaternion.Euler(0, 50, 0);
+        else
+        {
+            _isDraging = false;
+        }
+    }
+    void RotateObject()
+    {
+        // Получаем движение мыши по осям X и Y
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        
+        Vector3 rotationForvord = new Vector3(-mouseY, -mouseX, 0).normalized;
+        
+        transform.Rotate(rotationForvord, _rotationSpeed * Time.deltaTime, 0);
     }
 
-    private Vector2 Distance(Vector2 point1, Vector2 point2) 
-        => new Vector2( Mathf.Sqrt(Cube(point1.x-point2.x)), 
-                        Mathf.Sqrt(Cube(point2.y - point1.y)));
-    private float Cube(float value) => value * value; 
+    private void FixedUpdate()
+    {
+        if (_isDraging)
+        {
+            Vector3 rotation = this.gameObject.transform.eulerAngles;
+            _text.text = Distance(rotation).ToString();
+            
+            if (Distance(rotation) < m_valueRightDistance)
+            {
+                _text.text = "End Game";
+                _endGame = true;
+                StartCoroutine(EndGame());
+            }
+        }
+    }
+
+    private IEnumerator EndGame()
+    {
+        float time = m_endGameTimer;
+        float Timer = 0;
+        Vector3 toRotation = this.gameObject.transform.eulerAngles;
+        
+        while (Timer < time)
+        {
+            this.gameObject.transform.rotation=Quaternion.Euler(Vector3.Lerp(toRotation, m_fromTheRightAngle, Timer/time));
+            _material.SetColor("_Color", Color.Lerp(m_startColor, m_endColor, Timer/time));
+            yield return null;
+            Timer += Time.deltaTime;
+        }
+        this.gameObject.transform.rotation = Quaternion.Euler(m_fromTheRightAngle);
+    }
+
+    private float Distance( Vector3 to)
+    {
+        return Mathf.Abs((to - m_fromTheRightAngle).magnitude);
+    }
+
+
+    
 }
